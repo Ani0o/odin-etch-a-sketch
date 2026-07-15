52481d43d31364e6d6f60354693dafcf2a86524c
Ani <205335742+Ani0o@users.noreply.github.com>
2026-07-15 20:10:51 +0530

    Create a 16 x 16 grid of squares on the web page
    Added a parent div in the html file. Appended the squares to this div
    using a for loop in the js file. Made the parent div a flexbox to
    create a grid. Added the box sizing property to the squares because
    adding an border to them was creating inconsistencies with defining the
    height and width properties.

---
+++ script.js
@@ -0,0 +1,9 @@
+const grid = document.querySelector("#grid");
+
+grid.setAttribute("style", "height: 640px; width: 640px;")
+
+for (let i = 0; i < 256; i++) {
+    const square = document.createElement("div");
+    square.setAttribute("style", "height: 40px; width: 40px; border: 1px solid black; box-sizing: border-box;");
+    grid.appendChild(square);
+}
