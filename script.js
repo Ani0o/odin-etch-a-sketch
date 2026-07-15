a24e4cf71c6b489252dfa71d881425b33816c83a
Ani <205335742+Ani0o@users.noreply.github.com>
2026-07-15 21:49:51 +0530

    Add hover effect to draw on the grid
    Added a hover class in the css file and an event listener on each
    square. When the mouse pointer enters the element the hover class is
    added to the square to make its background color black.

--- script.js
+++ script.js
@@ -5,5 +5,10 @@ grid.setAttribute("style", "height: 640px; width: 640px;")
 for (let i = 0; i < 256; i++) {
     const square = document.createElement("div");
     square.setAttribute("style", "height: 40px; width: 40px; border: 1px solid black; box-sizing: border-box;");
+
+    square.addEventListener("mouseenter", (event) => {
+        square.classList.add("hover");
+    })
+    
     grid.appendChild(square);
 }
