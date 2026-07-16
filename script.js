d8c66004ca6e183c2c1bc62be0f4b2160e3b3a8f
Ani <205335742+Ani0o@users.noreply.github.com>
2026-07-16 17:53:03 +0530

    Add functionality to choose the grid size
    Added a button in the html file which will let user enter a grid size
    using prompt. Added an event listener on the button and moved the main
    logic inside it.

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 const grid = document.querySelector("#grid");
+const sizeButton = document.querySelector("#size-button");
 
-grid.setAttribute("style", "height: 640px; width: 640px;")
+const gridSize = "640px";
+grid.setAttribute("style", `height: ${gridSize}; width: ${gridSize};`);
 
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement("div");
-    square.setAttribute("style", "height: 40px; width: 40px; border: 1px solid black; box-sizing: border-box;");
+sizeButton.addEventListener("click", (event) => {
+    grid.replaceChildren();
 
-    square.addEventListener("mouseenter", (event) => {
-        square.classList.add("hover");
-    })
-    
-    grid.appendChild(square);
-}
+    let numberOfSquares = prompt("Enter the number of squares (1-100) : ", "1");
+    numberOfSquares = parseInt(numberOfSquares, 10);
+
+    if (numberOfSquares < 1 || numberOfSquares > 100) alert("Invalid Size!");
+    else {
+        let squareSize = parseInt(gridSize, 10) / numberOfSquares;
+        squareSize = String(squareSize) + "px";
+
+        for (let i = 0; i < numberOfSquares * numberOfSquares; i++) {
+            const square = document.createElement("div");
+            square.setAttribute("style", `height: ${squareSize}; width: ${squareSize}; border: 1px solid black; box-sizing: border-box;`);
+
+            square.addEventListener("mouseover", (event) => {
+                square.classList.add("hover");
+            })
+            
+            grid.appendChild(square);
+        }
+    }
+})
